6b9e1f4f8a4ec10d2426c2dba336a249c47ab20c
Célestin Matte <celestin.matte@gmail.com>
2015-07-25 19:00:03 +0200

    Clean database.sql from commented out code and add comments

--- database.sql
+++ database.sql
@@ -2,38 +2,17 @@ DROP DATABASE IF EXISTS n_scores2;
 CREATE DATABASE n_scores2;
 connect n_scores2;
 
--- CREATE TABLE `players` (
---        id int(10) NOT NULL AUTO_INCREMENT,
---        pseudo varchar(100) NOT NULL,
---        is_hacker int(1) DEFAULT 0,
---        is_cheater int(1) DEFAULT 0,
---        PRIMARY KEY (`id`)
--- );
-
--- id will be level name directly put into INT
--- e.g. "95-4" is 954
--- episodes will end in 5
-
--- CREATE TABLE `levels` (
---       id int(3) NOT NULL,
---       is_episode int(1),
---       PRIMARY KEY (`id`)
--- );
-
--- score stored into the "number of frames" form
-
+-- contains a copy of all score at every timestamp (download)
 CREATE TABLE `score` (
        level_id int(3) NOT NULL,
        pseudo varchar(100) NOT NULL,
---       player_id int(10) NOT NULL,
        `timestamp` timestamp DEFAULT CURRENT_TIMESTAMP,
        score int(10) unsigned NOT NULL,
        place int(1) NOT NULL,
        PRIMARY KEY (`level_id`, `pseudo`, `timestamp`)
---     CONSTRAINT `player_id_foreign` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)
---     CONSTRAINT `level_id_foreign` FOREIGN KEY (`level_id`) REFERENCES `levels` (`id`)
 );
 
+-- contains scores only once
 CREATE TABLE `score_unique` (
        level_id int(3) NOT NULL,
        pseudo varchar(100) NOT NULL,

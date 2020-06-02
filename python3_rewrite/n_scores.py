0343a7590993b53be8a842e2a2125d791589f05b
Célestin Matte <celestin.matte@gmail.com>
2020-06-02 21:15:48 +0200

    Remove global cursor to avoid apache crash

--- python3_rewrite/n_scores.py
+++ python3_rewrite/n_scores.py
@@ -17,7 +17,6 @@ app = Flask(__name__)
 
 app.debug = is_debug_activated
 con = None
-cur = None
 
 MIN_DATE = "January 20, 2006"
 
@@ -35,6 +34,18 @@ class Score:
     score = 0
     place = -1
 
+def execute(query, parameters = None, return_rowcount = False, return_rows = False):
+    cur = con.cursor()
+    cur.execute(query, parameters)
+    if return_rowcount:
+        res = cur.rowcount
+    elif return_rows:
+        res = cur.fetchall()
+    else:
+        res = cur.fetchone()
+    cur.close()
+    return res
+
 def connect_db():
     return mdb.connect('localhost', user, password, 'n_scores2')
 
@@ -43,12 +54,11 @@ def decimal_default(obj):
                 return float(obj)
     raise TypeError
 
-def disp_graph(cur):
+def disp_graph(rows):
     res = ""
     p = player()
     p.reset()
-    row = cur.fetchone()
-    while row is not None:
+    for row in rows:
         #if isinstance(row[0], int):
         name = html.escape(row[0])
         #else:
@@ -61,7 +71,6 @@ def disp_graph(cur):
         score.append(calendar.timegm(row[1].utctimetuple())*1000)
         score.append(row[2])
         p.data.append(score)
-        row = cur.fetchone()
         # last player
     res = res + "{ name: " + "'" + p.name + "'" + ", data:" + json.dumps(p.data, default=decimal_default) + "}"
     return res
@@ -98,8 +107,8 @@ def disp_player():
     try:
         diff_top20_0th = ""
         pseudo = request.args.get('pseudo', '')
-        cur.execute("SELECT timestamp, level_id, score FROM score_unique WHERE pseudo = %s AND timestamp=(SELECT MIN(timestamp) FROM score_unique WHERE pseudo = %s);", (pseudo, pseudo));
-        row = cur.fetchone()
+        rows = execute("SELECT timestamp, level_id, score FROM score_unique WHERE pseudo = %s AND timestamp=(SELECT MIN(timestamp) FROM score_unique WHERE pseudo = %s);", (pseudo, pseudo), return_rows = True);
+        row = rows[0]
         first_top_20 = ""
         first_top_20_timedelta = row[0]
         first_top_20_timestamp = row[0].strftime("%B %d, %Y")
@@ -107,16 +116,14 @@ def disp_player():
             first_top_20_timestamp = "before " + MIN_DATE
         else:
             first_top_20 = '(' + str(level_id_to_str(row[1])) + ', ' + score_to_str(row[2]) + ')'
-            row = cur.fetchone()
-            while (row is not None):
+            for row in rows:
                 first_top_20 += ', (' + str(level_id_to_str(row[1])) + ', ' + score_to_str(row[2]) + ')'
-                row = cur.fetchone()
 
-        cur.execute("SELECT timestamp, level_id, score FROM score_unique WHERE pseudo = %s AND place = 0 AND timestamp=(SELECT MIN(timestamp) FROM score_unique WHERE pseudo = %s AND place = 0);", (pseudo, pseudo));
+        rows = execute("SELECT timestamp, level_id, score FROM score_unique WHERE pseudo = %s AND place = 0 AND timestamp=(SELECT MIN(timestamp) FROM score_unique WHERE pseudo = %s AND place = 0);", (pseudo, pseudo), return_rows = True);
         first_0th = ""
         first_0th_timestamp = ""
-        row = cur.fetchone()
         # Players may have a top 20 but no 0th
+        row = rows[0]
         if row is not None:
             first_0th_timedelta = row[0]
             first_0th_timestamp = row[0].strftime("%B %d, %Y")
@@ -124,10 +131,8 @@ def disp_player():
                 first_0th_timestamp = "before " + MIN_DATE
             else:
                 first_0th = '(' + str(level_id_to_str(row[1])) + ', ' + score_to_str(row[2]) + ')'
-                row = cur.fetchone()
-                while (row is not None):
+                for row in rows:
                     first_0th += ', (' + str(level_id_to_str(row[1])) + ', ' + score_to_str(row[2]) + ')'
-                    row = cur.fetchone()
             diff_top20_0th = (first_0th_timedelta - first_top_20_timedelta).days
         else:
             first_0th_timestamp = "Never"
@@ -149,12 +154,11 @@ def display_all_score():
     player = request.args.get('player', '')
     place = request.args.get('place', '')
     if player == "" and place == "":
-        cur.execute("SELECT timestamp, pseudo, score, place, ISNULL(demo) FROM score_unique WHERE level_id = %s ORDER BY score DESC", (level_id))
+        execute("SELECT timestamp, pseudo, score, place, ISNULL(demo) FROM score_unique WHERE level_id = %s ORDER BY score DESC", (level_id))
     elif player != "":
-        cur.execute("SELECT timestamp, pseudo, score, place, ISNULL(demo) FROM score_unique WHERE level_id = %s AND pseudo = %s ORDER BY score DESC", (level_id, player))
+        execute("SELECT timestamp, pseudo, score, place, ISNULL(demo) FROM score_unique WHERE level_id = %s AND pseudo = %s ORDER BY score DESC", (level_id, player))
     else:
-        cur.execute("SELECT timestamp, pseudo, score, place, ISNULL(demo) FROM score_unique WHERE level_id = %s AND place = %s ORDER BY score DESC", (level_id, place))
-    rows = cur.fetchall()
+        rows = execute("SELECT timestamp, pseudo, score, place, ISNULL(demo) FROM score_unique WHERE level_id = %s AND place = %s ORDER BY score DESC", (level_id, place), return_rows = True)
     table = ""
     for row in rows:
         timestamp = str(row[0])
@@ -198,82 +202,71 @@ def disp_level():
         top_opt = " AND place <= " + str(top)
 
     # Number of player who ever got a top 20
-    cur.execute("SELECT pseudo FROM score_unique WHERE level_id = %s GROUP BY pseudo;", (converted_level,))
-    nb20 = cur.rowcount
+    nb20 = execute("SELECT pseudo FROM score_unique WHERE level_id = %s GROUP BY pseudo;", (converted_level,), return_rowcount = True)
 
     # Number of player who ever got a 0th
-    cur.execute("SELECT pseudo FROM score_unique WHERE level_id = %s AND place = 0 GROUP BY pseudo;", (converted_level,))
-    nb0 = cur.rowcount
+    nb0 = execute("SELECT pseudo FROM score_unique WHERE level_id = %s AND place = 0 GROUP BY pseudo;", (converted_level,), return_rowcount = True)
 
     if diff == str(1):
-        cur.execute("SELECT 'max(score) - min(score)', timestamp, (MAX(score) - min(score))*0.025 FROM score where level_id = %s group by timestamp", (converted_level,))
-        return render_template("all.html", series=disp_graph(cur),
+        rows = execute("SELECT 'max(score) - min(score)', timestamp, (MAX(score) - min(score))*0.025 FROM score where level_id = %s group by timestamp", (converted_level,), return_rows = True)
+        return render_template("all.html", series=disp_graph(rows),
                                level=level, by_place=by_place_form,
                                avg=avg_form, top=top_form, diff=diff_form,
                                nb20=nb20, nb0=nb0)
     if by_place != str(1):
         if avg == str(1):
-            cur.execute("SELECT pseudo, timestamp, score*0.025 FROM score WHERE level_id = %s" + top_opt + " UNION SELECT 'average score', timestamp, AVG(score)*0.025 from score where level_id = %s" + top_opt + " GROUP BY timestamp", (converted_level, converted_level))
+            rows = execute("SELECT pseudo, timestamp, score*0.025 FROM score WHERE level_id = %s" + top_opt + " UNION SELECT 'average score', timestamp, AVG(score)*0.025 from score where level_id = %s" + top_opt + " GROUP BY timestamp", (converted_level, converted_level), return_rows = True)
         else:
-            cur.execute("SELECT pseudo, timestamp, score*0.025 FROM score WHERE level_id = %s" + top_opt, (converted_level,))
+            rows = execute("SELECT pseudo, timestamp, score*0.025 FROM score WHERE level_id = %s" + top_opt, (converted_level,), return_rows = True)
     else:
         if avg == str(1):
-            cur.execute("SELECT place, timestamp, score*0.025 FROM score WHERE level_id = %s" + top_opt + " UNION SELECT 'average score', timestamp, AVG(score)*0.025 from score where level_id = %s" + top_opt + " GROUP BY timestamp ORDER BY place, timestamp", (converted_level, converted_level))
+            rows = execute("SELECT place, timestamp, score*0.025 FROM score WHERE level_id = %s" + top_opt + " UNION SELECT 'average score', timestamp, AVG(score)*0.025 from score where level_id = %s" + top_opt + " GROUP BY timestamp ORDER BY place, timestamp", (converted_level, converted_level), return_rows = True)
         else:
-            cur.execute("SELECT place, timestamp, score*0.025 FROM score WHERE level_id = %s " + top_opt + " ORDER BY place, timestamp;", (converted_level,))
-    return render_template("all.html", series=disp_graph(cur), level=level,
+            rows = execute("SELECT place, timestamp, score*0.025 FROM score WHERE level_id = %s " + top_opt + " ORDER BY place, timestamp;", (converted_level,), return_rows = True)
+    return render_template("all.html", series=disp_graph(rows), level=level,
                            by_place=by_place_form, avg=avg_form, top=top_form,
                            diff=diff_form, nb20=nb20, nb0=nb0)
 
 @app.route('/stats', methods=['POST', 'GET'])
 def disp_stats():
     # 0th table
-    cur.execute("SELECT COUNT(*), YEAR(timestamp) FROM score_unique WHERE place = 0 GROUP BY YEAR(timestamp);")
+    rows = execute("SELECT COUNT(*), YEAR(timestamp) FROM score_unique WHERE place = 0 GROUP BY YEAR(timestamp);", return_rows = True)
     table_0th = {}
-    row = cur.fetchone()
-    while row is not None:
+    for row in rows:
         table_0th[row[1]] = row[0]
-        row = cur.fetchone()
     scores_series = "{ name: '0th', data: ["
     for date in sorted(table_0th):
         scores_series += "[Date.UTC(" + str(date) + ", 0, 1), " + str(table_0th[date]) + "],"
     scores_series += "]},\n"
     # 20th table
-    cur.execute("SELECT COUNT(*), YEAR(timestamp) FROM score_unique GROUP BY YEAR(timestamp);")
+    rows = execute("SELECT COUNT(*), YEAR(timestamp) FROM score_unique GROUP BY YEAR(timestamp);", return_rows = True)
     table_20th = {}
-    row = cur.fetchone()
-    while row is not None:
+    for row in rows:
         table_20th[row[1]] = row[0]
-        row = cur.fetchone()
     scores_series += "{ name: '20th', data: ["
     for date in sorted(table_20th):
         scores_series += "[Date.UTC(" + str(date) + ", 0, 1), " + str(table_20th[date]) + "],"
     scores_series += "]},\n"
     # unique 0th table
-    cur.execute("SELECT COUNT(DISTINCT level_id), YEAR(timestamp) FROM score_unique WHERE place = 0 GROUP BY YEAR(timestamp);")
+    rows = execute("SELECT COUNT(DISTINCT level_id), YEAR(timestamp) FROM score_unique WHERE place = 0 GROUP BY YEAR(timestamp);", return_rows = True)
     table_unique_0th = {}
-    row = cur.fetchone()
-    while row is not None:
+    for row in rows:
         table_unique_0th[row[1]] = row[0]
-        row = cur.fetchone()
     scores_series += "{ name: 'Number of distinct levels whose 0th changed', data: ["
     for date in sorted(table_0th):
         scores_series += "[Date.UTC(" + str(date) + ", 0, 1), " + str(table_unique_0th[date]) + "],"
     scores_series += "]}"
     # Number of 0th
-    cur.execute("SELECT COUNT(pseudo) AS a, COUNT(DISTINCT pseudo), level_id FROM score_unique WHERE place = 0 GROUP BY level_id ORDER BY a DESC;")
+    rows = execute("SELECT COUNT(pseudo) AS a, COUNT(DISTINCT pseudo), level_id FROM score_unique WHERE place = 0 GROUP BY level_id ORDER BY a DESC;", return_rows = True)
     table = ""
-    row = cur.fetchone()
-    while row is not None:
+    for row in rows:
         level = level_id_to_str(row[2])
         table += "<tr><td>" + str(row[0]) + "</td><td>" + str(row[1]) + "</td><td><a href='/level?level=" + level + "&top=1'>" + level + "</a></td></tr>"
-        row = cur.fetchone()
     return render_template("stats.html", table=table, scores_series=scores_series)
 
 @app.route('/new', methods=['POST', 'GET'])
 def new():
-    cur.execute("SELECT level_id, pseudo, score, timestamp, place from score_unique where timestamp > now() - interval 1 month ORDER BY timestamp DESC")
-    rows = cur.fetchall()
+    rows = execute("SELECT level_id, pseudo, score, timestamp, place from score_unique where timestamp > now() - interval 1 month ORDER BY timestamp DESC", return_rows = True)
     table = ""
     for row in rows:
         level_id = str(row[0])
@@ -283,9 +276,8 @@ def new():
         timestamp = str(row[3])
         place = str(row[4])
         top = str(int(place) + 1)
-        cur.execute("SELECT score, timestamp, ISNULL(demo) from score_unique where level_id = %s and pseudo = %s and timestamp < %s ORDER BY timestamp DESC limit 1;", (level_id, pseudo, timestamp))
-        if cur.rowcount > 0:
-            row2 = cur.fetchone()
+        row = execute("SELECT score, timestamp, ISNULL(demo) from score_unique where level_id = %s and pseudo = %s and timestamp < %s ORDER BY timestamp DESC limit 1;", (level_id, pseudo, timestamp))
+        if row is not None:
             prev_score = score_to_str(row2[0])
             prev_date = str(row2[1])
             demo_exists = not bool(row2[2])
@@ -302,8 +294,7 @@ def new():
 
 @app.route('/latest0th', methods=['POST', 'GET'])
 def latest0th():
-    cur.execute("SELECT level_id, pseudo, timestamp, score, place FROM score_unique WHERE place = 0 ORDER BY timestamp DESC, level_id LIMIT 20;")
-    rows = cur.fetchall()
+    rows = execute("SELECT level_id, pseudo, timestamp, score, place FROM score_unique WHERE place = 0 ORDER BY timestamp DESC, level_id LIMIT 20;", return_rows = True)
     table = ""
     for row in rows:
         level_id = str(row[0])
@@ -313,9 +304,8 @@ def latest0th():
         score = score_to_str(row[3])
         place = str(row[4])
         top = str(int(place) + 1)
-        cur.execute("SELECT score, timestamp, ISNULL(demo) from score_unique where level_id = %s and pseudo = %s and timestamp < %s ORDER BY timestamp DESC limit 1;", (level_id, pseudo, timestamp))
-        if cur.rowcount > 0:
-            row2 = cur.fetchone()
+        row2 = execute("SELECT score, timestamp, ISNULL(demo) from score_unique where level_id = %s and pseudo = %s and timestamp < %s ORDER BY timestamp DESC limit 1;", (level_id, pseudo, timestamp))
+        if row2 is not None:
             prev_score = score_to_str(row2[0])
             prev_date = str(row2[1])
             demo_exists = not bool(row2[2])
@@ -338,10 +328,9 @@ def demo():
         timestamp = request.args.get('timestamp', '')
         score = request.args.get('score', '')
         if score == "":
-            cur.execute("SELECT demo FROM score_unique WHERE level_id = %s AND pseudo = %s AND timestamp = %s", (level_id, pseudo, timestamp))
+            row = execute("SELECT demo FROM score_unique WHERE level_id = %s AND pseudo = %s AND timestamp = %s", (level_id, pseudo, timestamp))
         else:
-            cur.execute("SELECT demo FROM score_unique WHERE level_id = %s AND pseudo = %s AND score = %s", (level_id, pseudo, score))
-        row = cur.fetchone()
+            row = execute("SELECT demo FROM score_unique WHERE level_id = %s AND pseudo = %s AND score = %s", (level_id, pseudo, score))
         return render_template("demo.html", demo=row[0])
     except:
         return render_template("error.html", error="Unable to find demo.")
@@ -353,11 +342,10 @@ def index():
 
 def display_all():
     res = ""
-    cur.execute("SELECT pseudo, timestamp, count(score) as c FROM score WHERE level_id < 1000 GROUP BY pseudo, timestamp HAVING c>200 ORDER BY pseudo, timestamp")
-    #cur.execute("select pseudo, count(*) as c from score, players where score.player_id=id and timestamp='2007-01-01 12:53:40' group by player_id order by c DESC LIMIT 10;")
+    rows = execute("SELECT pseudo, timestamp, count(score) as c FROM score WHERE level_id < 1000 GROUP BY pseudo, timestamp HAVING c>200 ORDER BY pseudo, timestamp", return_rows = True)
+    #execute("select pseudo, count(*) as c from score, players where score.player_id=id and timestamp='2007-01-01 12:53:40' group by player_id order by c DESC LIMIT 10;")
     p = player()
-    row = cur.fetchone()    
-    while row is not None:
+    for row in rows:
         #name = unicode(row[0], errors='ignore')
         name = html.escape(row[0])
         if name != p.name and p.name != "":
@@ -368,21 +356,17 @@ def display_all():
         score.append(calendar.timegm(row[1].utctimetuple())*1000)
         score.append(row[2])
         p.data.append(score)
-        row = cur.fetchone()
         # last player
     res = res + "{ name: " + "'" + p.name + "'" + ", data:" + json.dumps(p.data) + "}"
     return render_template("all.html", series=res)
 
 @app.before_request
 def before_request():
-    global con, cur
+    global con
     con = connect_db()
-    cur = con.cursor()
 
 @app.after_request
 def after_request(res):
-    global con, cur
-    cur.close()
     con.close()
     return res
 

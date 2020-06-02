3e5aa62183391380a184d79da85b32a9c2c7df34
Célestin Matte <celestin.matte@gmail.com>
2020-06-02 11:13:41 +0200

    Add python3_rewrite

---
+++ python3_rewrite/add_hacker.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+# Add a hacker in the config file and remove him from the database
+
+from nhighlib import *
+import datetime
+import sys
+import _mysql
+import MySQLdb as mdb
+import config
+
+def connect_db():
+    return mdb.connect('localhost', config.user, config.password, 'n_scores2')
+
+
+if __name__=='__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python add_hacker.py <name>")
+        sys.exit(1)
+
+    nhighconfig = NHighConfig()
+    if sys.argv[1] in nhighconfig.ignoredPlayers:
+        print("Hacker already known. Cleaning database anyway.")
+    else:
+        nhighconfig.ignoredPlayers.append(sys.argv[1])
+        nhighconfig.save()
+
+    config.con = connect_db()
+    cur = config.con.cursor()
+
+    cur.execute("SELECT level_id, timestamp, place, score FROM score_unique WHERE pseudo = %s order by level_id, timestamp", (sys.argv[1]))
+    rows = cur.fetchall()
+    nb_rows = len(rows)
+    i = 0
+    while i < nb_rows:
+        row = rows[i]
+        if i + 1 < nb_rows:
+            next_row = rows[i+1]
+        else:
+            next_row = None
+        level_id = row[0]
+        timestamp = row[1]
+        place = row[2]
+        score = row[3]
+        print("Cleaning level " + str(level_id))
+        if next_row != None and next_row[0] == level_id:
+            # If there are several scores on a level, only updates
+            # places until the date of that next score
+            cur.execute("UPDATE score_unique SET place = place - 1 WHERE level_id = %s AND timestamp >= %s AND place > %s AND score <= %s AND timestamp < %s", (level_id, timestamp, place, score, next_row[1]))
+            cur.execute("UPDATE score SET place = place - 1 WHERE level_id = %s AND timestamp >= %s AND place > %s AND score <= %s AND timestamp < %s", (level_id, timestamp, place, score, next_row[1]))
+        else:
+            cur.execute("UPDATE score_unique SET place = place - 1 WHERE level_id = %s AND timestamp >= %s AND place > %s AND score <= %s", (level_id, timestamp, place, score))
+            cur.execute("UPDATE score SET place = place - 1 WHERE level_id = %s AND timestamp >= %s AND place > %s AND score <= %s", (level_id, timestamp, place, score))
+        cur.execute("DELETE FROM score_unique WHERE pseudo = %s AND level_id = %s AND timestamp = %s", (sys.argv[1], level_id, timestamp))
+        i = i + 1
+
+    print("Cleaning table score")
+    cur.execute("DELETE FROM score WHERE pseudo = %s", (sys.argv[1]))
+    
+    config.con.commit()
+    config.con.close()
+
---
+++ python3_rewrite/auto_download.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env pythonw
+
+# Auto downloader
+# Downloads a new .hs file and saves it in current folder
+# Usage : python auto_download.py <file>
+# You need nhighlib.py from nhigh to use this script
+
+from nhighlib import *
+import datetime
+import sys
+import _mysql
+import MySQLdb as mdb
+import getopt
+
+import config
+
+cur_time = str(datetime.datetime.now())
+cur = None
+
+def connect_db():
+    return mdb.connect('localhost', config.user, config.password, 'n_scores2')
+
+def usage():
+    print("usage: " + sys.argv[0] + "[--fill-score][--fill-score-unique][--save-hs-file][--help]")
+    print("\t--fill-score: fill database with all scores. Don't do this too often (e.g. daily)")
+    print("\t--fill-score-unique: fill database with new scores only. Do this often (e.g. hourly)")
+    print("\t--save-hs-file: keep .hs file (readable by NHigh)")
+    print("\t--help: display this help message")
+
+def run():
+    global cur
+    if len(sys.argv) == 1:
+        usage()
+        sys.exit(1)
+    print("Downloading data from metanet server...")
+    downloader = HSDownloader()
+    table = []
+    for i in downloader:
+        print(i)
+    table = downloader.result()
+    config.con = connect_db()
+    cur = config.con.cursor()
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], '', ["fill-score", "fill-score-unique", "save-hs-file", "help"])
+    except getopt.GetoptError as err:
+        print("Error: " + str(err))
+        sys.exit(1)
+
+    for o, arg in opts:
+        if o == "--fill-score-unique":
+            print("Checking new demos...")
+            download_demos(table.table)
+        elif o == "--fill-score":
+            print("Adding all scores to table score...")
+            fill_score(table.table)
+        elif o == "--save-hs-file":
+            print("Saving data to file " + cur_time + ".hs")
+            saveScores(table, cur_time + ".hs")
+        elif o == "--help":
+            usage()
+            sys.exit(0)
+
+    config.con.close()
+
+
+def convert_level_nb(episode_nb, level_nb):
+    return episode_nb*10 + level_nb
+
+def download_demos(scores):
+    level_nb = 0
+    episode_nb = 0
+    for episode in scores:
+        for level in episode:
+            place = 0
+            for score in level:
+                if score.name != '':
+                    level_id = convert_level_nb(episode_nb, level_nb)
+                    if not demo_exists(level_id, score.name, score.score):
+                        print("New demo:", level_id, score.name, score.score)
+                        add_demo(episode_nb, level_nb, score.name, score.score, cur_time, place)
+                place = place + 1
+            level_nb = level_nb + 1
+        episode_nb = episode_nb + 1
+        level_nb = 0
+    config.con.commit()
+
+def add_demo(episode_nb, level_nb, pseudo, score, timestamp, place):
+    (player, score, demo) = downloadReplayByName(episode_nb, level_nb, pseudo)
+    if demo == None:
+        print("Error: Could not find demo for " + pseudo + " on level " + str(episode_nb) + "-" + str(level_nb), file=sys.stderr)
+    if player != pseudo:
+        print("Error: player name is not what was expected : %s instead of %s for level %d-%d" % (player, pseudo, episode_nb, level_nb))
+    else:
+        level_id = convert_level_nb(episode_nb, level_nb)
+        try:
+            cur.execute("INSERT INTO score_unique VALUES(%s, %s, %s, %s, %s, %s)", (level_id, pseudo, timestamp, score, place, demo))
+            config.con.commit()
+        except mdb.IntegrityError as err:
+            print(err)
+
+def demo_exists(level_id, pseudo, score):
+    cur.execute("SELECT 1 FROM score_unique WHERE level_id=%s AND pseudo=%s AND score=%s", (level_id, pseudo, score))
+    return (cur.rowcount > 0)
+
+def add_score(score, episode_nb, level_nb, timestamp, place):
+    try:
+        db_level_nb = convert_level_nb(episode_nb, level_nb)
+        cur.execute("INSERT INTO score VALUES(%s, %s, TIMESTAMP(%s), %s, %s)", (db_level_nb, score.name, timestamp, score.score, place))
+    except mdb.IntegrityError as err:
+        # There ARE duplicate entries on the scoreboard.
+        # It's sad, but the best is to ignore them.
+        pass
+
+def fill_score(scores):
+    level_nb = 0
+    episode_nb = 0
+    for episode in scores:
+        for level in episode:
+            place = 0
+            for score in level:
+                if score.name != '':
+                    add_score(score, episode_nb, level_nb, cur_time, place)
+                place = place + 1
+            level_nb = level_nb + 1
+        episode_nb = episode_nb + 1
+        level_nb = 0
+    config.con.commit()
+
+
+if __name__=='__main__':
+    run()
